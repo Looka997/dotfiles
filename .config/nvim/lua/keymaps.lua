02d68687610aa8aa8c0e68add2cd9e47c5f3d824
Looka997 <luca.marinelli997@gmail.com>
2024-10-12 17:36:15 +0200

    aggiugo configurazione nvim e .zshrc - imposto stow

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,27 @@
+-- [[ Basic Keymaps ]]
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Exit insert mode' })
+
+vim.keymap.set('n', '<leader>pv', ':Ex<CR>', { desc = 'Go to [P]roject [V]iew' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- [[ Basic Autocommands ]]
+-- Highlight when yanking (copying) text
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- vim: ts=2 sts=2 sw=2 et
